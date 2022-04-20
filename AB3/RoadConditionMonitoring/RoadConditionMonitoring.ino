35d6ade9a836b323535a2e82650e440384a9b91a
timlock <81235388+timlock@users.noreply.github.com>
2022-04-20 13:24:37 +0200

    AB3

---
+++ AB3/RoadConditionMonitoring/RoadConditionMonitoring.ino
@@ -0,0 +1,57 @@
+#include <M5Stack.h>
+#include <TinyGPS++.h>
+#include <MPU9250.h>
+#include "utility/MPU9250.h"
+
+using namespace std;
+
+String createTimeStamp();
+
+TinyGPSPlus gps;
+HardwareSerial serialStream(2);
+MPU9250 IMU;
+
+void setup() {
+  M5.begin();
+  M5.Power.begin();
+  serialStream.begin(9600);
+  Wire.begin();
+  IMU.initMPU9250(); // this line must be after Wire.begin()
+  M5.Lcd.println("Road Condition Monitoring\n");
+}
+
+void loop() {
+  M5.Lcd.setCursor(0, 20);
+    while(serialStream.available()>0){
+      Serial.println("Encode raw gps signal..");
+      gps.encode(serialStream.read());
+    }
+    uint32_t satelliteCount = gps.satellites.value();
+    int32_t hdopValue = gps.hdop.value();
+    double lattitude = gps.location.lat();
+    double longitude = gps.location.lng();
+    uint8_t days = gps.date.day();
+    uint8_t months = gps.date.month();
+    uint16_t years = gps.date.year();
+    String timeStamp = createTimeStamp();
+    M5.Lcd.println("Sats|HDOP|Latitude  |Longitude  |Date     |Time\n");
+    M5.Lcd.printf("%u   |%d|%f  |%f   |%u/%u/%u|%s\n\n",satelliteCount, hdopValue, lattitude, longitude, days, months, years, timeStamp.c_str());
+
+    M5.Lcd.println("x-Acceleration y-Acceleration z-Acceleration");
+
+    delay(17);
+}
+
+String createTimeStamp(){
+    String timeStamp = "";
+    uint8_t hours = (gps.time.hour() + 2) % 24;
+    uint8_t minutes = gps.time.minute();
+    uint8_t seconds = gps.time.second();
+    if(hours < 10) timeStamp += "0";
+    timeStamp += String(hours) + ":";
+    if(minutes < 10) timeStamp += "0";
+    timeStamp += String(minutes) + ":";
+    if(seconds < 10) timeStamp += "0";
+    timeStamp += String(seconds);
+    return timeStamp;
+}
